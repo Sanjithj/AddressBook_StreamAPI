e64ce01c51cfea9f3b8375ba27f276dd9fb8598a
Sanjithj <sanjithj591997@gmail.com>
2022-07-12 12:57:50 +0530

    Added: Add Multiple contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,11 +1,35 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Main");
-		ServiceBook.addContact();
-		ServiceBook.display();
-		ServiceBook.editContact();
-		ServiceBook.deleteContact();
+		Scanner sc =new Scanner(System.in);
+		int option;
+		while(true) {
+			System.out.println("Operation want to perform\n 1)Add Contact\n 2)edit contact\n 3)delete\n4)Display contact");
+			option =sc.nextInt();
+			switch(option) {
+			case 1:
+				ServiceBook.addContact();
+				break;
+			case 2:
+				ServiceBook.editContact();
+				break;
+			case 3:
+				ServiceBook.deleteContact();
+				break;
+			case 4:
+				ServiceBook.display();
+				break;
+			default:
+				System.out.println("Enter the option");
+			}
+			
+		System.out.println("Visit again to website");
+		sc.close();
+		
+		}
 	}
 }
--- ServiceBook.java
+++ ServiceBook.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ServiceBook {
 	static Scanner sc=new Scanner(System.in);
 	static ArrayList<Contacts> arrayOfContacts = new ArrayList<Contacts>();
-	public static void addContact() {
+	public static Contacts addContact() {
 		System.out.println("Please enter your first name :");
 	    String first_name = sc.next();
 	    System.out.println("Please enter your last name :");
@@ -25,7 +25,7 @@ public class ServiceBook {
 	    System.out.println("Please enter your email id :");
 	    String email = sc.next();
 	    Contacts contact=new Contacts(first_name,last_name,Address,city,state,zip,phone_number,email);
-	    arrayOfContacts.add(contact);
+	    return contact;
 	    
 	}
 	public static int edit(String name) {
